bc2fa518723832f49bbe09138975049ba97bc655
Rajdeepc <rajrock38@gmail.com>
2020-04-19 00:40:25 +0530

    ui

--- frontend/src/common-components/NavBar.vue
+++ frontend/src/common-components/NavBar.vue
@@ -2,17 +2,29 @@
   <b-navbar toggleable type="dark" variant="dark">
     <b-navbar-brand to = '/'>Url Shortener</b-navbar-brand>
     <b-navbar-nav class="ml-auto">
-      <b-button-group>
+      <b-button-group v-show="!isLoggedIn">
         <b-button to = '/registration' variant="primary">Sign In</b-button>
         <b-button to = '/registration' variant="success">Sign Up</b-button>
       </b-button-group>
+
+      <b-button-group v-show="isLoggedIn">
+        <b-button to = '/profile' variant="primary">My Profile</b-button>
+        <b-button to = '/' variant="danger">Log Out</b-button>
+      </b-button-group>
+
+
     </b-navbar-nav>
   </b-navbar>
 </template>
 
 <script>
 export default {
-  name: "Navbar"
+  name: "Navbar",
+  data(){
+    return {
+      isLoggedIn: false
+    }
+  }
 };
 </script>
 
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,9 +1,11 @@
 import Registration from './components/Registration/Registration.vue'
 import Home from './components/Home/Home.vue'
+import Profile from './components/Profile/Profile.vue'
 
 
 const Routes = [
     { path: '/', component: Home },
+    { path: '/profile', component: Profile },
     { path: '/registration', component: Registration }
   ]
 
